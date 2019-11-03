6687825e62dc5858c045f8e93fdec1b871bda430
oscaros <komuntuoscar@gmail.com>
2019-11-03 22:04:02 +0300

    initial commit3

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public http: HttpClient) {
+    this.saveSelectedToCloud();
+  }
+
+  saveToCloud(): Observable<any>{  
+     let host = "free-football-soccer-videos.p.rapidapi.com"
+      let key = "7d4d0b9f01msh989a5287e61be53p111fb1jsn4b5d899df206";
+      let headers = new HttpHeaders({'x-rapidapi-host': host, 'x-rapidapi-key': key});   
+      return this.http.get('https://free-football-soccer-videos.p.rapidapi.com/', {headers:headers});
+   }
+  
+  saveSelectedToCloud(){
+      this.saveToCloud().subscribe(res=>{
+        if (res.error) throw new Error(res.error);
+        console.log(res);
+      })   
+  } 
 
 }
