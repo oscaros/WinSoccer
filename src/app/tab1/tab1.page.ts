c953497b56861f128922c24af8225f1dd8bd40b2
oscaros <komuntuoscar@gmail.com>
2019-11-23 09:55:49 +0300

    changes in sources

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DomSanitizer } from '@angular/platform-browser';
+import {NavController} from '@ionic/angular'
 
 
 @Component({
@@ -13,14 +14,19 @@ export class Tab1Page {
 
   arrayVideoFeed: any [];
   //  NewsAPI = require('newsapi');
-  sourcesArray=[ "bbc-sport", "four-four-two", "talksport", "the-sport-bible"]
+  sourcesArray=[ "goal.com", "football365.com", "101greatgoals.com", "fourfourtwo.com",
+                  "teamtalk.com", "90min.in", "soccernews.com", "caughtoffside.com", "sportslens.com",
+                  "mlssoccer.com", "arseblog.com"]
+
+  //today1 = "ff";
   
-
+// current= new Date('2011/05/24');
  // url = 'https://newsapi.org/v2/everything?q=soccer&sources=bbc-sport,four-four-two,talksport&from=2019-11-14&to=2019-11-14&sortBy=popularity&language=en&apiKey=bb27f1860f904aa681bb7bb4f478db50'
-  url = 'https://newsapi.org/v2/everything?q=news&sources='
-  urlend= '&from=2019-11-13&to=2019-11-14&sortBy=popularity&language=en&apiKey=bb27f1860f904aa681bb7bb4f478db50'
+  url = 'https://newsapi.org/v2/everything?domains='
+  //urlend= '&from='+today1+'&to='+today1+'&sortBy=popularity&language=en&apiKey=bb27f1860f904aa681bb7bb4f478db50'
+urlend= '&from=2019-11-20&to=2019-11-22&sortBy=publishedAt&language=en&apiKey=bb27f1860f904aa681bb7bb4f478db50'
 
-  constructor(public http: HttpClient, public sanitizer: DomSanitizer) {
+  constructor(public http: HttpClient, public sanitizer: DomSanitizer, public navCtrl: NavController) {
     // const newsapi = this.NewsAPI('bb27f1860f904aa681bb7bb4f478db50');
     
     // newsapi.v2.everything({
@@ -37,6 +43,17 @@ export class Tab1Page {
     
     // });
 
+ 
+
+
+   /* today = new Date();
+  dd = String(today.getDate()).padStart(2, '0');
+  mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  yyyy = today.getFullYear();  
+  today = yyyy + '-' + mm + '-' + dd;
+  console.log(today);*/
+  //today1 = today;
+
 
     var i: number;
   for (i = 0; i < this.sourcesArray.length; i++) {   
@@ -48,17 +65,12 @@ export class Tab1Page {
         }else{
           this.arrayVideoFeed = res.articles.concat(this.arrayVideoFeed)
         }
-
-        
       }
-      
-      
-    
       console.log(res);
       
     })   
   }
-  }
+}
 
   getVideoFeed(): Observable<any>{  
      let host = "free-football-soccer-videos.p.rapidapi.com"
@@ -68,7 +80,7 @@ export class Tab1Page {
    }
 
    getNewsFeed(source): Observable<any>{  
-    let host = "free-football-soccer-videos.p.rapidapi.com"
+    let host = "NewsAPIraygorodskijV1.p.rapidapi.com"
      let key = "7d4d0b9f01msh989a5287e61be53p111fb1jsn4b5d899df206";
      let headers = new HttpHeaders({'x-rapidapi-host': host, 'x-rapidapi-key': key});  
      console.log(source); 
@@ -76,7 +88,47 @@ export class Tab1Page {
      
   }
 
-  
+  viewDetail(){
+    this.navCtrl.navigateRoot('newsdetail');
+  }
+
+
+
+ public timeDifference(current, previous) {
+    
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+    
+    var elapsed = current - previous;
+    
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+    
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+    
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+         return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+    
+    else if (elapsed < msPerYear) {
+         return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+    
+    else {
+         return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
+
   
 
 }
